0f45cc1a33c8c6e0846af8406965ade3eaf38243
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2023-03-10 21:17:55 +0100

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main (String[] args) {
         
         JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-        window.setTitle("2D Game");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+        window.setResizable(false); // Window size constant
+        window.setTitle("2D Game"); // Window title
 
-        GamePanel gamePanel = new GamePanel();
+        GamePanel gamePanel = new GamePanel(); 
         window.add(gamePanel);
 
         window.pack();
