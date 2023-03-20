ca67db1d2d49e2d921aeaeb16b8a0014b35ba259
Al Bel <albellebvas@gmail.com>
2023-03-20 20:46:24 +0100

    Signed-off-by: Al Bel <albellebvas@gmail.com>

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -13,14 +13,21 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
-    Thread gameThread; 
-
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    
+    // Set player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black); // bg colour
         this.setDoubleBuffered(true); // better rendering performance
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
 
     }
 
@@ -47,6 +54,19 @@ public class GamePanel extends JPanel implements Runnable {
 
     public void update() {
 
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+        }
+        else if (keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if (keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if (keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+
     }
 
     public void paintComponent(Graphics g) {
@@ -58,7 +78,7 @@ public class GamePanel extends JPanel implements Runnable {
 
         g2.setColor(Color.white); // Our object
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose(); // Memory saving technique
     }
