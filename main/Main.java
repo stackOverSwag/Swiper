6d31435c2a8f6955523995b0b51315685428c0a0
Alex Belenkiy <51411108+stackOverSwag@users.noreply.github.com>
2023-03-24 23:48:15 +0100

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -18,6 +18,11 @@ public class Main {
         window.setVisible(true);
 
         gamePanel.startGameThread(); // starts the game!
+        
+        // TODO
+        // Enemies
+        // Prolly inventory???
+        // MAPPP
 
     }
 }
